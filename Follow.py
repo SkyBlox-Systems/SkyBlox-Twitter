10e3dff93e838689b40a71893fd408bf98b94774
SkyBlox <xxhackerminecraftpe2@gmail.com>
2020-10-20 06:34:07 +0100

    Update Follow.py

--- Follow.py
+++ Follow.py
@@ -16,4 +16,4 @@ for follower in followers:
     if num == 1:
         follower.follow()
         print('I just followed ' + follower.screen_name + '!')
-        time.sleep(120)
+        time.sleep(3600)
