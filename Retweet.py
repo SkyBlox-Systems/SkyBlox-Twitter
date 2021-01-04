4609aa44c48864647ee61506daefdad0ab13f207
SkyBlox <xxhackerminecraftpe2@gmail.com>
2021-01-04 15:45:26 +0000

    Update Retweet.py

--- Retweet.py
+++ Retweet.py
@@ -1,23 +1,23 @@
 import tweepy
 import time
 
-auth = tweepy.OAuthHandler('m7bxRtNvSoiEESjWZs1JseKfz','8rY9YQvTSzznViUa5MoGti1JzfZZBleVlP5LagZlLKjPCktmfl')
-auth.set_access_token('1211753853185449984-NIshQh4KEiTs951sLyteptaqG6DBvG','gOKUHK8xKVGoVTdBNnAtdldBNGRGSIgawFyY14fQuQ2pL')
+auth = tweepy.OAuthHandler('2pZBE81oZ9KGjOn28By6SzblQ','L5q73xBiVWMWnr7YuZktnHw5rVO8qVkwce29ZJ1s7XJhdrSCR6')
+auth.set_access_token('1211753853185449984-jGhuOp7NrnVkAN7AZYjvutWMyBhOn9', 'jFRTcj6h0szfhXQdVV57yMov6OadnDo7StB7Niu8gpDUf')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-search = 'robloxDev', 'roblox'
-nrTweets = 2000
+ROBLOX = 'robloxDev', '#RobloxDev'
+nrTweets = 750
 
-for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
+for tweet in tweepy.Cursor(api.search, ROBLOX).items():
     try:
-        print('tweet retweeted')
         tweet.retweet()
-        time.sleep(60)
+        print('tweet retweeted')
+        time.sleep(25)
     except tweepy.TweepError as e:
-        time.sleep(30)
+        time.sleep(5)
         print(e.reason)
     except StopIteration:
         break
